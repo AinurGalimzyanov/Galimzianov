052956d72a73a3e8011152e265629533bc98a2dd
Galimzyanov Ainur <a.galimz@mail.ru>
2022-12-30 23:02:45 +0500

    небольшие изменения

--- task_3_4_2.py
+++ task_3_4_2.py
@@ -3,10 +3,10 @@ import numpy as np
 from openpyxl import Workbook
 from openpyxl.styles import Font, Border, Side
 from openpyxl.utils import get_column_letter
+from openpyxl.styles.numbers import FORMAT_PERCENTAGE_00
 from jinja2 import Environment, FileSystemLoader
 import pdfkit
 import pandas as pd
-from openpyxl.styles.numbers import FORMAT_PERCENTAGE_00
 
 
 class DataSet:
@@ -17,23 +17,23 @@ class DataSet:
         vacancies_objects (list): Список вакансий
     """
 
-    def __init__(self, file_name):
+    def __init__(self):
         """
         Конструктор для инициализация объекта DataSet, который создает поле для хранения списка вакансий
         Args:
              file_name (str): Название файла
         """
-        self.file_name = file_name
         self.salary_by_year = dict()
         self.vacancies_count_by_year = dict()
         self.salary_by_profession_name = dict()
         self.vacancies_count_by_profession_name = dict()
+        self.salary_by_city = dict()
+        self.vacancy_rate_by_city = dict()
         self.dict_lict = list()
 
     def return_list(self):
         return [self.salary_by_year, self.vacancies_count_by_year, self.salary_by_profession_name,
-                self.vacancies_count_by_profession_name]
-
+                self.vacancies_count_by_profession_name, self.salary_by_city, self.vacancy_rate_by_city]
 
 class InputConnect:
     """Класс для ввода данных и формирования отчетности о вакансиях
@@ -60,19 +60,44 @@ class InputConnect:
         :param self: Объект класса InputConnect
         :param data: Объект класса DataSet
         """
-        df = pd.read_csv(data.file_name)
-        df['salary'] = df['salary'].fillna(0)
-        df['salary'] = df['salary'].astype("int64")
+        df = pd.read_csv(self.file_name)
+        count = len(df)
+        df["salary"] = df[["salary_from", "salary_to"]].mean(axis=1)
+        df["count"] = df.groupby("area_name")["area_name"].transform("count")
+        df_norm = df[df["count"] > 0.01 * count]
+        df_area = df_norm.groupby("area_name", as_index=False)["salary"].mean().sort_values(by="salary", ascending=False)
+        df_area["salary"] = df_area["salary"].apply(lambda x: int(x))
+        df_area10 = df_area.head(10)
+        data.salary_by_city = dict(zip(df_area10["area_name"], df_area10["salary"]))
+
+        data.vacancy_rate_by_city = {k: round(v / count, 4) for k, v in dict(df["area_name"].value_counts()).items()}
+
+        print(f"Динамика уровня зарплат по годам: {data.salary_by_year}")
+        print(f"Динамика количества вакансий по годам: {data.vacancies_count_by_year}")
+        print(f"Динамика уровня зарплат по годам для выбранной профессии: {data.salary_by_profession_name}")
+        print(
+            f"Динамика количества вакансий по годам для выбранной профессии: {data.vacancies_count_by_profession_name}")
+        print(f"Уровень зарплат по городам (в порядке убывания): {data.salary_by_city}")
+        print(f"Доля вакансий по городам (в порядке убывания): {dict(list(data.vacancy_rate_by_city.items())[:10])}")
+
+    def read_csv_by_path(self, path: str):
+        """Метод для многопоточной обработки csv файлов при помощи модуля pandas
+        :param path: Путь до csv файла с вакансиями
+        :param data: Объект класса DataSet
+        """
+        df = pd.read_csv(path)
+        df["salary"] = df[["salary_from", "salary_to"]].mean(axis=1)
         df["published_at"] = df["published_at"].apply(lambda d: int(d[:4]))
-        years = df["published_at"].unique()
+        year = df["published_at"][0]
         df_vacancy = df["name"].str.contains(self.profession_name)
 
-        for year in years:
-            filter_by_year = df["published_at"] == year
-            data.salary_by_year[year] = int(df[filter_by_year]["salary"].mean())
-            data.vacancies_count_by_year[year] = len(df[filter_by_year])
-            data.salary_by_profession_name[year] = int(df[df_vacancy & filter_by_year]["salary"].mean())
-            data.vacancies_count_by_profession_name[year] = len(df[df_vacancy & filter_by_year])
+        filter_by_year = df["published_at"] == year
+        salary_by_year = (year, int(df[filter_by_year]["salary"].mean()))
+        vacancies_count_by_year = (year, len(df[filter_by_year]))
+        salary_by_profession_name = (year, int(df[df_vacancy & filter_by_year]["salary"].mean()))
+        vacancies_count_by_profession_name = (year, len(df[df_vacancy & filter_by_year]))
+        return salary_by_year, vacancies_count_by_year, salary_by_profession_name, vacancies_count_by_profession_name
+
 
 
 class Report:
@@ -89,7 +114,7 @@ class Report:
         """
         Инициализирует объект класса Report
         """
-        self.dataSet_list = DataSet("vacancies.csv").return_list()
+        self.dataSet_list = DataSet().return_list()
         self.workbook = Workbook()
         self.workbook.remove(self.workbook.active)
         self.sheet_year = self.workbook.create_sheet("Статистика по годам")
@@ -234,14 +259,13 @@ class Report:
                     self.sheet_city.cell(row, col).value = str(round(self.sheet_city.cell(row, col).value * 100, 2)) + '%'
 
         pdf_template = Environment(loader=FileSystemLoader('.'))\
-            .get_template("pdf_template.html").render({'name': 'Программист', 'png': "graph.png",
-                                        'sheet_year':  self.sheet_year,
-                                        'sheet_city': self.sheet_city})
+            .get_template("pdf_template_3_4_2.html").render({'name': 'Программист', 'png': "graph.png",
+                                        'sheet_year':  self.sheet_year})
         config = pdfkit.configuration(wkhtmltopdf=r'E:\загрузки\wkhtmltopdf\bin\wkhtmltopdf.exe')
-        pdfkit.from_string(pdf_template, 'report.pdf', configuration=config, options={'enable-local-file-access': None})
+        pdfkit.from_string(pdf_template, 'report_3_4_2.pdf', configuration=config, options={'enable-local-file-access': None})
 
 
-inputparam = InputConnect()
-dataset = DataSet(inputparam.file_name)
-InputConnect.print_data_dict(inputparam, dataset)
-Report().generate_pdf()
+if __name__ == '__main__':
+    inputparam = InputConnect()
+    dataset = DataSet()
+    Report().generate_pdf()
